945560cde8661d8d9c2c1030bacc6486a3befe7d
Victor Andrade de Oliveira <victor.oliveira@cloudpay.net>
2022-12-01 10:41:03 +0100

    feat: init FCM service when platform is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { FcmService } from './services/fcm.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private fcmService: FcmService
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.fcmService.initPush();
+    });
+  }
 }
